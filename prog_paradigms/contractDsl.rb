2b593b683f988699e1a15b6302fc7b7ae0724563
Noé <No@MacBook-Pro-de-Noe-Phan.local>
2013-05-16 21:55:04 +0200

    added comments to contractdsl.rb

--- prog_paradigms/contractDsl.rb
+++ prog_paradigms/contractDsl.rb
@@ -10,8 +10,8 @@ class ContractDSL
         @liableMethod = nil
         @sourceFile = nil #the source file in which the class is defined
         @mySexp = nil #current sexp on which we check the contract
-        @condition = nil #the application condition for require and sugest
-        @singleStatement = nil #check condition on single statement or entire body
+        @condition = nil #the application condition of the contract
+        @singleStatement = nil #to check condition on single statement or entire body
     end
     
     def contractName(aName)
@@ -36,12 +36,16 @@ class ContractDSL
         res = instance_eval(&block)
     end
     
+    #checks if the liable class and liable method exist in the source file
+    #pre: @mySexp must be set
     def validLiables()
+        #check the liable class
         aClass = @mySexp.getClassSexp(@liableClass.to_sym)
         if aClass == nil
             p "there exists no class #{@liableClass} in #{@sourceFile}"
             return false
         end
+        #check the liable method
         method = @mySexp.getMethodSexp(@liableClass.to_sym, @liableMethod)
         if method == nil
             p "there exists no method #{@liableMethod} in the class #{@liableClass}"
@@ -57,21 +61,23 @@ class ContractDSL
         if not validLiables()
             return false
         end
+        #evaluate the application conditions
         @condition = instance_eval(&block)
     end
 
-
+    #evaluates the clauses of the contract
+    #the contract is respected if 
+    #   - the block is respected 
+    #        or
+    #   - the block is not respected but the application 
+    #   condition is not respected
     def require(&block)
         #check if the liable class and the liable methods exist
         if not validLiables()
             return false
         end
         res = instance_eval(&block)
-        #the contract is respected if 
-        #   the block is respected 
-        #   or
-        #   the block is not respected but the application 
-        #   condition is not respected
+        
         if @condition == nil
             #behaviour when no application conditions are specified
             if res
@@ -90,28 +96,35 @@ class ContractDSL
         return res
     end
     
+    #negates a condition
     def neg(&block)
         res, msg = instance_eval(&block)
         return (not res)
     end
     
-   
+    #checks if the first statement of the liable method respect a condition in &block
     def beginsWith(&block)
         first = @mySexp.getFirstStatement(@liableClass.to_sym, @liableMethod)
         @mySexp = first
+        #enable the analysis of a single statement (used by genericCheck())
         @singleStatement = true
         res = instance_eval(&block)
     end
     
+    #checks if the last statement of the liable method respect a condition in &block
     def endsWith(&block)
         last = @mySexp.getLastStatement(@liableClass.to_sym, @liableMethod)
         @mySexp = last
-        p @mySexp
+        #enable the analysis of a single statement (used by genericCheck())
         @singleStatement = true
         res = instance_eval(&block)
     end
     
-    
+    #generic method to check if the liable method or statement contains 
+    #a specific statement type
+    #var: - "atype" is a symbol representing an statement type in the s-expressions
+    #     - "anExpression" can be a symbol representing a variable or method, 
+    #        or a complete s-expression
     def genericCheck(aType, anExpression)
         if @singleStatement
              body = @mySexp
@@ -151,33 +164,39 @@ class ContractDSL
     
     end
     
+    #check if the liable method or statement assigns to a specific variable
     def assigns?(aVariable)
         return genericCheck(:lasgn, aVariable)
     end
     
+    #check if the liable method or statement calls a specific method
     def calls?(aMethod)
         return genericCheck(:call, aMethod)
     end
     
+    #check if the liable method or statement return a specific expression
     def returns?(anExpression)
         expression = RubyParser.new.parse(anExpression) 
         return genericCheck(:return, expression)
     end
     
+    #check if the liable method or statement does a super send
     def doesSuperSend?()
         return genericCheck(:zsuper, nil)
     end
-        
+    
+    #check if the liable method or statement does a self
     def doesSelfSend?()
         return genericCheck(:self, nil)
     end
 
+    #check if the liable method or statement contains a while statement
     def while?()
         return genericCheck(:while, nil)
     end
 
     
-    # returns true if the liable method in the liable class is overriden
+    #checks if the liable method in the liable class is overriden
     def isOverriden?()
         found = false
         c = Object.const_get(@liableClass).new
@@ -195,7 +214,7 @@ class ContractDSL
         return found
     end
     
-    #returns true if the method aMethod is implemented in the class of the liable method
+    #check if the method aMethod is implemented in the class of the liable method
     def isImplemented?()
         c = Object.const_get(@liableClass).new
         res = c.class.instance_methods(false).include?(@liableMethod.to_s)
