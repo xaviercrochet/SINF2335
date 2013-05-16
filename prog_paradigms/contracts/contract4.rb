818aa1affecee4f8eecd906084acec827d9fe474
Xavier Crochet <xacrochet@gmail.com>
2013-05-16 14:27:01 +0200

    Added report skeleton & updated .gitignore

---
+++ prog_paradigms/contracts/contract4.rb
@@ -0,0 +1,15 @@
+c = ContractDSL.new
+c.define {
+	conractName "Big Contract"
+	inAClass "BigExample"
+	definedIn "examples/example_big.rb"
+	forAMethod :m1
+
+	inCondition {
+
+	}
+
+	require {
+		
+	}
+}
